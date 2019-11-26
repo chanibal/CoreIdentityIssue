6bcb8dad3aa9491cb0f1763ceafd20eaed7c1ee8
Krzysztof Bociurko <git@chanibal.pl>
2019-11-26 09:40:34 +0100

    Newly created ASP.NET MVC project
    Created new project, ASP.NET Core Web Application (Model-View-Controller)
    
    Authentication: Individual User Accounts, store user accounts in-app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNetCore_CoreIdentity_user_data_issue.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
